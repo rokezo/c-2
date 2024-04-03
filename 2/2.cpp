aac81c1c6199d5803d286ad2bf36af03dc0ed0ea
rokezo <dobusnikita@gmail.com>
2024-04-03 23:15:53 +0300

    Добавьте файлы проекта.

---
+++ 2/2.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int total = 0;
+    for (int i = 1; i <= 100; ++i) {
+        total += i;
+    }
+    std::cout << "Сума чисел від 1 до 100: " << total << std::endl;
+
+}
---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,10 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << i << std::endl;
+}
+
